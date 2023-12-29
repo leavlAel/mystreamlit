a9e6a30e8d212371d0d0855535e55a3081ff3f8b
leavlAel <154126093+leavlAel@users.noreply.github.com>
2023-12-29 14:32:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-import streamlit as st
-import requests
-from bs4 import BeautifulSoup
-import re
+import streamlit as st  
+from pyecharts import options as opts  
+from pyecharts.charts import Bar  
 import jieba
+import requests
 from collections import Counter
 from wordcloud import WordCloud
-import base64
-from PIL import Image
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib  
-matplotlib.rc('font', family='SimHei')
-
+from bs4 import BeautifulSoup
+import re 
+from pyecharts.charts import WordCloud
+from pyecharts.charts import Pie  
+from pyecharts.charts import Scatter
+from pyecharts.charts import Line    
+from pyecharts.charts import Radar 
+from pyecharts.charts import Funnel
 
 
 url = st.text_input("请输入url")
 # 筛选图形的侧边栏
 st.sidebar.subheader('图形筛选')
-graph_type = st.sidebar.selectbox('请选择图形类型', ['词云', '条形图', '饼图', '直方图', '散点图', '线图', '雷达图'])
+graph_type = st.sidebar.selectbox('请选择图形类型', ['词云', '条形图', '饼图', '横向柱状图', '散点图', '线图', '漏斗图'])
 response = requests.get(url)
+response.encoding=response.apparent_encoding
 # 解析HTML并提取body标签中的内容和标签  
 soup = BeautifulSoup(response.content, 'html.parser')  
 div = soup.find('div', {'id': 'UCAP-CONTENT'})
@@ -43,110 +45,62 @@ keys = list(sorted_dict.keys())[:20]
 values = [sorted_dict[key] for key in keys]
 words_count = dict(zip(keys,values))
 
-
 if graph_type == '词云':
-    #将词频字典转换为列表，以便wordcloud可以处理
-    words = list(words_count.keys())
-    counts = list(words_count.values())
-    #创建词云
-    wordcloud = WordCloud(font_path='SimHei.ttf',width=800,height=800,background_color='white').generate_from_frequencies(dict(zip(words,counts)))
-    #在matplotlib中显示词云
-    fig = plt.figure(figsize=(8, 8), facecolor=None)
-    plt.imshow(wordcloud)
-    plt.axis("off")
-    plt.tight_layout(pad=0)
-    #在streamlit中显示图像
-    st.subheader('绘制词云图')
-    st.pyplot(fig)
+    wordcloud = WordCloud()  
+    wordcloud.add("", list(words_count.items()), word_size_range=[20, 100], shape='triangle')  
+    wordcloud.set_global_opts(title_opts=opts.TitleOpts(title="词云图"))  
+    wordcloud.render("wordcloud.html")  
+    # 在Streamlit应用中显示词云图
+    st.components.v1.html(open('wordcloud.html', 'r', encoding='utf-8').read(), height=600,width=850)
 elif graph_type == '条形图':
-    # 将字典转换为列表，以便条形图可以处理
-    words = list(words_count.keys())
-    counts = list(words_count.values())
-    # 在matplotlib中创建条形图
-    fig = plt.figure(figsize=(10, 6))
-    plt.bar(words, counts)
-    plt.xlabel('Words')
-    plt.ylabel('Counts')
-    plt.title('Word Count')
-    # 在streamlit中显示图像
-    st.subheader('绘制条形图')
-    st.pyplot(fig)
+    bar = Bar()  
+    bar.add_xaxis(list(words_count.keys()))  
+    bar.add_yaxis("频次", list(words_count.values()))  
+    bar.set_global_opts(title_opts=opts.TitleOpts(title="条形图"),xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=30)))  
+    bar.render("bar_chart.html") 
+    # 在Streamlit应用中显示条形图  
+    st.components.v1.html(open('bar_chart.html', 'r', encoding='utf-8').read(), height=600,width=850) 
 elif graph_type == '饼图':
-    # 将字典转换为列表，以便饼图可以处理
-    words = list(words_count.keys())
-    counts = list(words_count.values())
-    # 在matplotlib中创建饼图
-    fig, ax = plt.subplots(figsize=(6, 6))
-    ax.pie(counts, labels=words, autopct='%1.1f%%', startangle=90)
-    # 设置中心文本
-    centre_circle = plt.Circle((0,0),0.70,fc='white')
-    fig = plt.gcf()
-    fig.gca().add_artist(centre_circle)
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    # 在streamlit中显示图像
-    st.subheader('绘制饼图')
-    st.pyplot(fig)
-elif graph_type == '直方图':
-    # 将字典转换为列表，以便直方图可以处理
-    bins = list(words_count.keys())
-    counts = list(words_count.values())
-
-    # 在matplotlib中创建直方图
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.hist(bins, bins=bins, weights=counts)
-    # 设置轴标签和标题
-    ax.set_xlabel('Value')
-    ax.set_ylabel('Frequency')
-    ax.set_title('Histogram of Data')
-    # 在streamlit中显示图像
-    st.subheader('绘制直方图')
-    st.pyplot(fig)
+    pie = Pie()  
+    pie.add("", [list(z) for z in zip(words_count.keys(), words_count.values())])  
+    pie.set_global_opts(title_opts=opts.TitleOpts(title="饼图"))  
+    pie.render("pie_chart.html")  
+    # 在Streamlit应用中显示饼图  
+    st.components.v1.html(open('pie_chart.html', 'r', encoding='utf-8').read(), height=600,width=850)
 elif graph_type == '散点图':
-    # 将字典转换为列表，以便散点图可以处理
-    keys = list(words_count.keys())
-    values = list(words_count.values())
-
-    # 在matplotlib中创建散点图
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.scatter(keys, values)
-    # 设置轴标签和标题
-    ax.set_xlabel('Variable 1')
-    ax.set_ylabel('Variable 2')
-    ax.set_title('Scatter Plot of Data')
-    # 在streamlit中显示图像
-    st.subheader('绘制散点图')
-    st.pyplot(fig)
+    scatter = Scatter()  
+    scatter.add_xaxis(list(words_count.keys()))  
+    scatter.add_yaxis('频次',list(words_count.values()))  
+    scatter.set_global_opts(title_opts=opts.TitleOpts(title="散点图"),xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=30)))  
+    scatter.render("scatter_chart.html")   
+    # 在Streamlit应用中显示散点图  
+    st.components.v1.html(open('scatter_chart.html', 'r', encoding='utf-8').read(), height=600,width=850) 
+elif graph_type == '横向柱状图':
+    bar = Bar()
+    bar.add_xaxis(list(words_count.keys()))
+    bar.add_yaxis("", list(words_count.values()))
+    bar.reversal_axis()
+    # 生成HTML文件
+    bar.render('histogram_chart.html')
+    # 在Streamlit应用中显示直方图  
+    st.components.v1.html(open('histogram_chart.html', 'r', encoding='utf-8').read(), height=600,width=850)
 elif graph_type == '线图':
-    # 将字典转换为列表，以便线图可以处理
-    keys = list(words_count.keys())
-    values = list(words_count.values())
-    # 在matplotlib中创建线图
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.plot(keys, values)
-    # 设置轴标签和标题
-    ax.set_xticks(range(len(keys)))
-    ax.set_xticklabels(keys)
-    ax.set_xticks(keys)
-    ax.set_xlabel('Time or Sequence')
-    ax.set_ylabel('Data Value')
-    ax.set_title('Line Plot of Data')
-    # 在streamlit中显示图像
-    st.subheader('绘制线图')
-    st.pyplot(fig)
-elif graph_type == '雷达图':
-    # 将字典转换为列表和数组，以便雷达图可以处理
-    categories = list(words_count.keys())
-    values = np.array(list(words_count.values()))
-
-    # 在matplotlib中创建雷达图
-    fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
-    ax.fill(values.T, color='blue', alpha=0.3)
-    ax.set_yticklabels(categories)
-    # 设置轴标签和标题
-    ax.set_xlabel('Word Count')
-    ax.set_title('Radar Plot of Word Frequencies')
-    # 在streamlit中显示图像
-    st.subheader('绘制雷达图')
-    st.pyplot(fig)
-st.subheader('词频排名前20的词汇')
-st.write('', ', '.join(words_count.keys()))
+    # 将数据转化为两个列表，一个为x轴，一个为y轴  
+    x_data = list(words_count.keys())  
+    y_data = list(words_count.values())  
+    # 创建一个线图对象  
+    line_chart = Line()  
+    # 添加x轴和y轴的数据，以及线的类型  
+    line_chart.add_xaxis(x_data)  
+    line_chart.add_yaxis("数值", y_data, is_smooth=True)  # is_smooth=True 表示曲线平滑  
+    # 设置图的全局配置项，例如标题和图例等  
+    line_chart.set_global_opts(title_opts=opts.TitleOpts(title="线图"),xaxis_opts=opts.AxisOpts(axislabel_opts=opts.LabelOpts(rotate=30)))
+    line_chart.render("line_chart.html")
+    # 在Streamlit应用中显示线图  
+    st.components.v1.html(open('line_chart.html', 'r', encoding='utf-8').read(), height=600,width=850)
+elif graph_type == '漏斗图': 
+    funnel = Funnel()
+    funnel.add("", list(words_count.items()))
+    funnel.render('funnel.html')
+    # 在Streamlit应用中显示线图  
+    st.components.v1.html(open('funnel.html', 'r', encoding='utf-8').read(), height=600,width=850)
